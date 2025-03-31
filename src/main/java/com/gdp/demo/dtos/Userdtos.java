8c63009941dc03655caa7d504f9a81e988c3c7ae
gdpatil39 <gdpatil39@gmail.com>
2025-03-31 11:49:11 +0530

    third commit validation complted initial

--- src/main/java/com/gdp/demo/dtos/Userdtos.java
+++ src/main/java/com/gdp/demo/dtos/Userdtos.java
@@ -1,5 +1,8 @@
 package com.gdp.demo.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -9,16 +12,16 @@ import lombok.*;
 public class Userdtos {
     private String userId;
 
-
+    @Size(min =3, max=15, message= "invalid Name")
     private String name;
-
+    @Email(message = "Invalid email")
     private String email;
-
+    @NotBlank(message = "password required")
     private String password;
 
-
+    @Size(min =3, max=15, message= "invalid Name")
     private String gender;
-
+    @Size(min =3, max=150, message= "invalid Name")
     private String about;
 
 
