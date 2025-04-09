78d4dbffb55c7d42b827da2122f6e709a4f3a402
gdpatil39 <gdpatil39@gmail.com>
2025-04-09 15:39:02 +0530

    MethodArgumentNotValidException added

--- src/main/java/com/gdp/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gdp/demo/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.gdp.demo.exceptions;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,5 +32,25 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity(response,HttpStatus.NOT_FOUND);
 		
 	
+	}
+	
+	
+	//MethodArgumentNotValidException
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String,Object>> handleMethodArgumentNotValidException(org.springframework.web.bind.MethodArgumentNotValidException ex){
+		
+		List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+		Map<String, Object> responce = new HashMap<>();
+		allErrors.stream().forEach(ObjectError -> {
+			String message=ObjectError.getDefaultMessage();
+			String field = ((FieldError)ObjectError).getField();
+			responce.put(field, message);
+		
+		});
+		
+	
+		return new ResponseEntity<>(responce, HttpStatus.BAD_REQUEST);
+		
+		
 	}
 }
