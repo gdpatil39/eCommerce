eacc6ff9948a45ca392e0d03d5e0c19fe267bb99
gdpatil39 <gdpatil39@gmail.com>
2025-04-11 13:57:03 +0530

    image upload added

---
+++ readme.txt
@@ -0,0 +1,12 @@
+fileupload
+http://localhost:8080/users/image/2a6a58ea-9b02-4be9-9a6d-dd7412be417c
+http://localhost:8080/users/image/{userId}                     
+Post
+
+body 
+form_data
+
+
+key = userImage
+
+file image.png
--- src/main/java/com/gdp/demo/controller/UserController.java
+++ src/main/java/com/gdp/demo/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.gdp.demo.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gdp.demo.dtos.ApiResponseMessage;
+import com.gdp.demo.dtos.ImageResponse;
 import com.gdp.demo.dtos.PageableResponse;
 import com.gdp.demo.dtos.Userdtos;
+import com.gdp.demo.services.FileService;
 import com.gdp.demo.services.UserService;
 
 import jakarta.validation.Valid;
@@ -29,6 +34,13 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private FileService fileService;
+	
+	@Value("${user.profile.image.path}")
+	private String imageUploadPath;
+	
+	
 	
 	//create
 	@PostMapping
@@ -107,6 +119,30 @@ public class UserController {
 			return new ResponseEntity<>(userService.searchUser(keywords),HttpStatus.OK);
 			
 		}
+		
+		//upload user image
+		@PostMapping("/image/{userId}")
+		public ResponseEntity<ImageResponse>uploadUserImage(@RequestParam("userImage") MultipartFile image,@PathVariable String userId) throws IOException{
+			
+			
+			String imageName =fileService.uploadFile(image, imageUploadPath);
+			
+			
+			//updating user_image_name in user table
+			Userdtos user = userService.getUserById(userId);
+			user.setImageName(imageName);
+			Userdtos userDto = userService.updateUser(user, userId);
+			
+			
+			ImageResponse imageResponse=ImageResponse.builder().imageName(imageName).success(true).status(HttpStatus.CREATED).build();
+			return new ResponseEntity<>(imageResponse,HttpStatus.CREATED);
+			
+			
+		}
+		
+		//serve user image 
+		
+		
 		
 		
 		
