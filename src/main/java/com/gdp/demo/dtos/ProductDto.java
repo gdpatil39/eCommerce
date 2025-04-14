cb9d465147c9e57be6c0f9c1f98c6886e362290c
gdpatil39 <gdpatil39@gmail.com>
2025-04-14 13:59:36 +0530

    Product entity and dto redy

---
+++ src/main/java/com/gdp/demo/dtos/ProductDto.java
@@ -0,0 +1,29 @@
+package com.gdp.demo.dtos;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductDto {
+
+	private String productId;
+	private String title;
+	
+	private String description;
+	private int price;
+	private int dicountedPrice;
+	private int qantity;
+	private Date addedDate;
+	private boolean live;
+	private boolean stock;
+}
